578e78d23833339ced38d21f21a2569386b281f2
Oliver Zott <zott_oliver@web.de>
2021-01-15 13:42:19 +0100

    Implementing first template annotation example

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,13 +5,29 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class QuestionController
 {
 
+    /**
+     * @return Response
+     * @Route("/")
+     */
     public function homepage(): Response
     {
         return new Response('First Controller test :)');
+    }
 
+    /**
+     * @param string $input
+     * @return Response
+     * @Route("/questions/{input}")
+     */
+    public function show(string $input): Response
+    {
+        return new Response(sprintf(
+            'Here is question: "%s".', ucwords(str_replace("-", " ", $input))
+        ));
     }
 }
