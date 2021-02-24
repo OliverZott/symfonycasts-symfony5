2f44133a1c49b5b4e5d902bf3d87c23b84adc5e6
OliverZott <zott_oliver@web.de>
2021-02-24 14:58:46 +0100

    Fooling around with service-configurations

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -25,6 +25,8 @@ class MarkdownHelper
      */
     public function parse(string $source): string
     {
+        dump($this->cache);
+
         return $this->cache->get(
             'markdown_' . md5($source),
             function () use ($source) {
